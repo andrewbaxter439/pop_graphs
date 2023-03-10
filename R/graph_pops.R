b3852a42f85d0b3ed8748a3f47faf850682a4b48
Andy Baxter <Andy.Baxter@glasgow.ac.uk>
2023-03-10 13:24:08 +0000

    Initial commit (safely this time)

---
+++ R/graph_pops.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+
+population <- read_csv("data/lfs_pop.csv", skip = 8,
+                       col_names = c("Year", "Population.England.Wales"))
+
+
+ggplot(population, aes(Year, Population.England.Wales)) +
+  geom_col()
+
+
+pop_tidy <- population |>
+  mutate(pop = Population.England.Wales * 1000) |> 
+  separate(Year, c("year", "month"), sep = " ", convert = TRUE) |> 
+  filter(str_detect(month, "\\w{3}"),
+         year > 2012) |> 
+  mutate(date = ym(paste(year, month)))
+
+pop_tidy |> 
+  ggplot(aes(date, pop)) +
+  geom_line()
+
+
+ggsave("graphs/pop_by_month.png")
